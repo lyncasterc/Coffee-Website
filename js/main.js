366ecf250874a9b84520a0ce554badaf66c4ff65
stivencabrera3 <stiven.cabrera3@gmail.com>
2019-05-15 03:01:18 -0400

    add js folder, add true js parallax effect on index

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Mobile Navbar
+let openSlideMenu = function() {
+  document.getElementById('side-menu').style.width = '250px'
+  // document.getElementById('main').style.marginRight = '250px'
+  document.getElementById('main').style.overflow = 'hidden'
+
+}
+
+let closeSlideMenu = function() {
+  document.getElementById('side-menu').style.width = '0px'
+  // document.getElementById('main').style.marginRight = '0px'
+  document.getElementById('main').style.overflow = 'scroll'
+}
+
+
+
+
+// Parallax Effect
+
+
+const parallax = document.querySelectorAll(".parallax");
+        
+  window.addEventListener("scroll", function() {
+      
+      let offset = window.pageYOffset;
+      
+      parallax.forEach(function(prllx, i) {
+      
+          prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
+      })
+  })
